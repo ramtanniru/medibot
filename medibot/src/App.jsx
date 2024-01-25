13e9ea2114c2ce3489ac026da4aa506d56c6c91d
sujannani <sujan.kommalapati@gmail.com>
2024-01-25 16:38:07 +0530

    initial commit

--- medibot/src/App.jsx
+++ medibot/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import About from './pages/about';
+import Home from './pages/home';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import SignupRecruitee from './pages/signupRecruitee';
+import { RecruiterPage } from './pages/recruiterPage';
+import { RecruiteePage } from './pages/recruiteePage';
+import Profile from './pages/profile';
+import Hrprofile from './pages/hrProfile';
+import HrJobs from './pages/JobsHR';
+import Jobs from './pages/jobs';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/recruiteeSignup' element={<SignupRecruitee/>}></Route>
+          <Route path='/recruiterPage/:id' element={<RecruiterPage/>}></Route>
+          <Route path='/recruiteePage/:id' element={<RecruiteePage/>}></Route>
+          <Route path='/recruiteePage/profile/:id' element={<Profile/>}></Route>
+          <Route path='/recruiterPage/hrProfile/:id' element={<Hrprofile/>}></Route>
+          <Route path='/recruiterPage/jobs/:id' element={<HrJobs/>}></Route>
+          <Route path='/recruiteePage/jobs/:id' element={<Jobs/>}></Route>
+          <Route path='/recruiteePage/about/:id' element={<About/>}></Route>
+        </Routes>
+      </BrowserRouter> 
+    </div>
+  );
 }
 
-export default App
+export default App;
